ab829d7cdde9cd4b61616a29a98afa3b41a1a2d4
dustinbroussard <dustin.broussard@gmail.com>
2025-07-17 07:07:40 -0500

    Add ThemeProvider context and wrap app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { ThemeProvider } from './hooks/useTheme';
 
 // Remove PWA register for now to fix build error
 // import { registerSW } from 'virtual:pwa-register'
 // registerSW({ immediate: true })
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(
+  <ThemeProvider>
+    <App />
+  </ThemeProvider>
+);
